f9bc0d9929c751a97f3f9f4d0ef17cbc4c6810b6
vanchous <vanyaloboda.89@gmail.com>
2019-11-24 22:34:38 +0200

    commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,21 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.runner.JUnitCore.runClasses;
+
+public class Runner {
+    public static void main(String[] args) {
+        Result result = runClasses(TestClass.class);
+
+        if (result.wasSuccessful()) {
+            System.out.println("Success!");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.err.println("Exception - " + failure.getException());
+                System.err.println("Trace - " + failure.getTrace());
+            }
+
+        }
+    }
+}
